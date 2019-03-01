9f33471601184b2f3217c678a15c23f43641df4b
Shams Imam <shams.imam@gmail.com>
2019-03-01 09:40:51 -0600

    uses clojure 1.10 compatible voom

--- project.clj
+++ project.clj
@@ -23,7 +23,9 @@
                  [cheshire "5.5.0"]]
 
   :plugins [[info.sunng/lein-bootclasspath-deps "0.3.0"]
-            [lein-voom "0.1.0-20171225_233657-g7962d1d"]]
+            [lein-voom "0.1.0-20180617_140646-g0ba7ec8" :exclusions [org.clojure/core.logic]]
+            ;; 0.8.10 of core.logic is not clojure 1.10 compatible
+            [org.clojure/core.logic "0.8.11"]]
   :boot-dependencies [[org.mortbay.jetty.alpn/alpn-boot "8.1.3.v20150130"
                        :prepend true]]
 
